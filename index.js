22114d96f4d513536ad06363ad92e0a5e8891fe8
vladimir <vladimir@blockscout.com>
2023-11-20 13:28:06 +0400

    upd

--- index.js
+++ index.js
@@ -1,64 +1,59 @@
-import TronWeb from 'tronweb'
+import TronWeb from "tronweb";
+import {writeFileSync} from  "fs";
+async function sendTx(tronWeb,pk) {
+  // const POOL_ADDRESS = "TLwZ4pSisGWtvdDpZyVd62kLELyZWV4mwN";
+  const POOL_ADDRESS = "TC85iyvzxktk54xNNUSfzyx8mr8hcZzPaM";
 
-async function main() {
-  const POOL_ADDRESS = "TLwZ4pSisGWtvdDpZyVd62kLELyZWV4mwN";
   const METHOD = "transact()";
-  const pk = process.env.PK;
+  
   //https://nile.tronscan.org/#/transaction/d795f99980ab921dd3dae7cb87c0e4af47fb3d93c8777d26f80f801bab341cfb
   const data =
-    "0xaf9890831cac637d434006c6b4cd1eb10b2d1c265436f16a064278e76d5c05705b039e0d1b6606e17a3f765031d64fc48ba3a751e5e97f3c9a4a6e0e534197f2350eb35900000000cc000000000000000000000000000000000000000000c35029c3028739c78aefb66fe0fe21009b13c5dbf273dc0f09f392ef28f3af6bc2e81c56deb94f2994658afd8c5336352a8b4263b71e424ebc5219b4e74a6c26fdcf2206f366727953ed1f321b564a38434d05791a7544c00337ea5b2bee8e6d3e8123b6aaf4c7f873430798f4ef9c2111bbf5fd992b486ce5f8c31ec52a2feb055922daee81403fb43f9fc7182b4879e1150b353add5cee8bc621184f0f3ba9b0860647c2ff7f7f97b2d49c6785b5d0f93680607c75860f345d1144f6ea2e900eb62ec1c8c9d175cad1c46a2ff0d2b091b742cf52de474220558526f3d93b07f358271a2ade1f795564bc2960412dfd869da2e101463c78921cc2805a9134dbf59822cff5e3f00f348d68fb8dd11027f4bfad0c15db0e7d71072da4be3677e6367517201197e443d5e2ea16aa2ef7eaf3fa83af81ea88f59cd00991e6df75e9467218312b3fedd7f91e1c29382c4d69b3f26fe7c738b9a6c74ada0af57f3d6dd5ad223ae2225efc58e6e79f28ba009e01582d3bfd482c0c11616dbe0dc7c5304afa127fb98718735a24b7c838ffb9bbc19fd8d3062528302869d77a42a0bde127b40ef97da24ee0c5d7ed597951c48cb19ab3082ef87cc098601236033d0f8e985d16614d39bea5f7b59effbd3cf933bc2348bbd40ac23b298bc65fd0d7ebd986f02cdfca1752a81ca0833054b478738864db5c8a77d8b6aab7bbadd100c3595fa92815ba4195d237db13e97cb9e257708919998278a7081e58e6cc8f93da87ef9e000000d2000000000000000001000000700500776e0142afa43ccb6a925bcf57977094e863ba5d266e57a17e7c655423c9d4adcc0b1d93cbad49364c7df5fa6d3c78ed9036a4d14acb68739ef5451408aac7995115500352a8ab77ad312ae1be622f7f1ef7ed6356e87232e2c5f68970296df9d733e26a1f30303fabbe260043c9c0deadbc3bc0e9c2ff50a212255fdb7b383d5d17305898d2afa3b5aca843b6a38d6a19e8cd8257a9fe4f5a183b5125bb284f1de9f736d477f423a14b1cf6f3b9db7beb61ecb36018ba5fcd56d21b829e930fa6d68facd8aac90e96f60dc7a828d1b9cfc1e4ec0163494ab84cc71c58301524136f9e0dc3adad20b189d1af9c78e9b63d809385297384e3dc125270ed5dfa81828bf7";
+    "0xaf9890831492b2b652a5cc5e079977739a205ab983b8103d8740a472d3ac245d07c2ca721477b9a45e4546b576e8f6110f08b0f1e492e0997bc0983661b618f4cd34eaac000000000000000000000000000000000000000000000000004c4b40178383e29eeb55fa2876b6891ae8e0ed95632a67bd853bb51bf51492e1922ce320299fc915a0f225330a5223c5ed5751ddf10866215b3bde81a3803a12e5661b0ad360eab1f74c33fa2f9faff47f0b61a0af3ac08728f7e22d611ca2bc55bfd7153f99a84c87ce788173a48953425a7dca0af1d64e2d8fc22d837d7790fe2e501e16604e1449ef73a41b596bd615078c936db50fe5dabf69dc73a8e129197d360a16c70db6258b4b352751164bf80cbea1325940ec05ad091e0438b109504dda248a916e9f13ac54a5ddba2013f8b8f97b795ad49c0047c5583bbe5f59e93cfb1a0f4091ed08cc6ea95e7a783730022afffe36848b69d37d694766bf80e0dfad2f7f71a06858f319f1ce1576b67097999ea57b58632ef5b68de1c08c2357749600c8a9feedc7b4508743ff2024b7d44d806911ac39997ee23994fb166e9c858023f048e6d07a16119822b970f1eab0f8b396672983ccf9b7c39aa1c57d64e6140559bf7294ee13613f48fea603d9cb39a51f05bd8dfb0753fb29ef777bc32ab222c609afe98e6298595238ce58a29814914a8cf63775d53c51725533d64058c4057deff7b0710f88c34c8dbd04aa206070dfb8e7b02e0917df1da5a576a062e42887d2ae9ce86aca86f5e239a7c9370aa7e884e01dc6586ead65bb4d1f5f505c033b5dc8468b10467d0512ce17c2332a58999acd7f152711bb2ed9f319a6f83e29aee18fe39106b99f9f84a6077f57b4485136c24351d51d1c34f9c668ebad22000000d200000000000000000100000041aa3c0bbb458e25a5699dca3db3ee3a587bc7a7229c758b5e2cbe572def3619af033f7afc0f14cf86ff58f6c82caa19b6de6a5b87383714eb014500ec0c8715c750a323334677175ecc1bfd06eae4d98b8dda3553c59ce9ba1e3927a2b9536e9647537328d8cfdf5dd719c9d813949c866228b69681d6dec39f2f39d4797a1866c4e87586c0c2451eb03942d853addb4434a5f79204e5d323d357620b8474d2810239d41ebc1cf051e5cb3e2daedf2c0c496e66e0f2fb4444a5111daabf60a5964ae1a2e4afba8ecd3e5cdb04110ca16e0a4a138f46e0ff7e5e55adc2e14d7b63c155a7c0cd30b5148635f4038caad51dbafb890807dc318a6b7829a9d812bb86bbc99c0506";
 
-    /*
-    Tx selector     : transact (0xaf989083)
-Nullifier       : 0x1cac637d434006c6b4cd1eb10b2d1c265436f16a064278e76d5c05705b039e0d
-Commitnment     : 0x1b6606e17a3f765031d64fc48ba3a751e5e97f3c9a4a6e0e534197f2350eb359
-Index           : 52224 (0xcc00)
+  /*
+   Tx selector     : transact (0xaf989083)
+Nullifier       : 0x1492b2b652a5cc5e079977739a205ab983b8103d8740a472d3ac245d07c2ca72
+Commitnment     : 0x1477b9a45e4546b576e8f6110f08b0f1e492e0997bc0983661b618f4cd34eaac
+Index           : 0 (0x0)
 Energy delta    : 0 Gwei (0x0)
-Token delta     : 50,000 Gwei (0xc350)
-Tx proof        : 29c3028739c78aefb66fe0fe21009b13c5dbf273dc0f09f392ef28f3af6bc2e8
-                  1c56deb94f2994658afd8c5336352a8b4263b71e424ebc5219b4e74a6c26fdcf
-                  2206f366727953ed1f321b564a38434d05791a7544c00337ea5b2bee8e6d3e81
-                  23b6aaf4c7f873430798f4ef9c2111bbf5fd992b486ce5f8c31ec52a2feb0559
-                  22daee81403fb43f9fc7182b4879e1150b353add5cee8bc621184f0f3ba9b086
-                  0647c2ff7f7f97b2d49c6785b5d0f93680607c75860f345d1144f6ea2e900eb6
-                  2ec1c8c9d175cad1c46a2ff0d2b091b742cf52de474220558526f3d93b07f358
-                  271a2ade1f795564bc2960412dfd869da2e101463c78921cc2805a9134dbf598
+Token delta     : 5,000,000 Gwei (0x4c4b40)
+Tx proof        : 178383e29eeb55fa2876b6891ae8e0ed95632a67bd853bb51bf51492e1922ce3
+                  20299fc915a0f225330a5223c5ed5751ddf10866215b3bde81a3803a12e5661b
+                  0ad360eab1f74c33fa2f9faff47f0b61a0af3ac08728f7e22d611ca2bc55bfd7
+                  153f99a84c87ce788173a48953425a7dca0af1d64e2d8fc22d837d7790fe2e50
+                  1e16604e1449ef73a41b596bd615078c936db50fe5dabf69dc73a8e129197d36
+                  0a16c70db6258b4b352751164bf80cbea1325940ec05ad091e0438b109504dda
+                  248a916e9f13ac54a5ddba2013f8b8f97b795ad49c0047c5583bbe5f59e93cfb
+                  1a0f4091ed08cc6ea95e7a783730022afffe36848b69d37d694766bf80e0dfad
 
-Tree proof      : 17201197e443d5e2ea16aa2ef7eaf3fa83af81ea88f59cd00991e6df75e94672
-                  18312b3fedd7f91e1c29382c4d69b3f26fe7c738b9a6c74ada0af57f3d6dd5ad
-                  223ae2225efc58e6e79f28ba009e01582d3bfd482c0c11616dbe0dc7c5304afa
-                  127fb98718735a24b7c838ffb9bbc19fd8d3062528302869d77a42a0bde127b4
-                  0ef97da24ee0c5d7ed597951c48cb19ab3082ef87cc098601236033d0f8e985d
-                  16614d39bea5f7b59effbd3cf933bc2348bbd40ac23b298bc65fd0d7ebd986f0
-                  2cdfca1752a81ca0833054b478738864db5c8a77d8b6aab7bbadd100c3595fa9
-                  2815ba4195d237db13e97cb9e257708919998278a7081e58e6cc8f93da87ef9e
+Tree proof      : 00c8a9feedc7b4508743ff2024b7d44d806911ac39997ee23994fb166e9c8580
+                  23f048e6d07a16119822b970f1eab0f8b396672983ccf9b7c39aa1c57d64e614
+                  0559bf7294ee13613f48fea603d9cb39a51f05bd8dfb0753fb29ef777bc32ab2
+                  22c609afe98e6298595238ce58a29814914a8cf63775d53c51725533d64058c4
+                  057deff7b0710f88c34c8dbd04aa206070dfb8e7b02e0917df1da5a576a062e4
+                  2887d2ae9ce86aca86f5e239a7c9370aa7e884e01dc6586ead65bb4d1f5f505c
+                  033b5dc8468b10467d0512ce17c2332a58999acd7f152711bb2ed9f319a6f83e
+                  29aee18fe39106b99f9f84a6077f57b4485136c24351d51d1c34f9c668ebad22
 
-New Merkle Root : 22cff5e3f00f348d68fb8dd11027f4bfad0c15db0e7d71072da4be3677e63675
+New Merkle Root : 2f7f71a06858f319f1ce1576b67097999ea57b58632ef5b68de1c08c23577496
 Tx type         : Deposit (0)
 Memo size       : 210 bytes
 ----------------------------------- MEMO BLOCK -----------------------------------
 Tx fee          : 0 (0x0)
 Items number    : 1
-Account hash    : 700500776e0142afa43ccb6a925bcf57977094e863ba5d266e57a17e7c655423
-A_p             : c9d4adcc0b1d93cbad49364c7df5fa6d3c78ed9036a4d14acb68739ef5451408
-Encrypted keys  : aac7995115500352a8ab77ad312ae1be622f7f1ef7ed6356e87232e2c5f68970
-                  296df9d733e26a1f30303fabbe260043
-Encrypted acc   : c9c0deadbc3bc0e9c2ff50a212255fdb7b383d5d17305898d2afa3b5aca843b6
-                  a38d6a19e8cd8257a9fe4f5a183b5125bb284f1de9f736d477f423a14b1cf6f3
-                  b9db7beb61ecb36018ba5fcd56d21b829e930fa6d68f
+Account hash    : 41aa3c0bbb458e25a5699dca3db3ee3a587bc7a7229c758b5e2cbe572def3619
+A_p             : af033f7afc0f14cf86ff58f6c82caa19b6de6a5b87383714eb014500ec0c8715
+Encrypted keys  : c750a323334677175ecc1bfd06eae4d98b8dda3553c59ce9ba1e3927a2b9536e
+                  9647537328d8cfdf5dd719c9d813949c
+Encrypted acc   : 866228b69681d6dec39f2f39d4797a1866c4e87586c0c2451eb03942d853addb
+                  4434a5f79204e5d323d357620b8474d2810239d41ebc1cf051e5cb3e2daedf2c
+                  0c496e66e0f2fb4444a5111daabf60a5964ae1a2e4af
 ----------------------------------------------------------------------------------
-ECDSA signature: acd8aac90e96f60dc7a828d1b9cfc1e4ec0163494ab84cc71c58301524136f9e
-                 0dc3adad20b189d1af9c78e9b63d809385297384e3dc125270ed5dfa81828bf7
-Deposit spender: 0xa9feabf9f8c3ee41eaa8cfdc6bc8960ffb837ef5 (recovered from ECDSA)
+ECDSA signature: ba8ecd3e5cdb04110ca16e0a4a138f46e0ff7e5e55adc2e14d7b63c155a7c0cd
+                 30b5148635f4038caad51dbafb890807dc318a6b7829a9d812bb86bbc99c0506
+Deposit spender: 0x02749c317b5145abfaab8cef264b27c4f9d53f75 (recovered from ECDSA)
      */
-  const tronWeb = new TronWeb(
-    "https://nile.trongrid.io",
-    "https://nile.trongrid.io",
-    "https://nile.trongrid.io"
-  );
-  const callerAddress = tronWeb.address.fromPrivateKey(pk)
-    // Workaround for https://github.com/tronprotocol/tronweb/issues/90
-  tronWeb.setAddress(callerAddress)
+  
   const txDesc = {
     to: POOL_ADDRESS,
     value: 0,
@@ -66,7 +61,7 @@ Deposit spender: 0xa9feabf9f8c3ee41eaa8cfdc6bc8960ffb837ef5 (recovered from ECDS
     METHOD,
   };
   const options = {
-    feeLimit: 200000000,
+    feeLimit: 600 * 1e6,
     callValue: txDesc.value,
     rawParameter: txDesc.data.slice(10), // TODO: cut off selector
   };
@@ -81,6 +76,40 @@ Deposit spender: 0xa9feabf9f8c3ee41eaa8cfdc6bc8960ffb837ef5 (recovered from ECDS
   return res;
 }
 
+
+
+
+function main() {
+    const tronWeb = new TronWeb(
+        "https://nile.trongrid.io",
+        "https://nile.trongrid.io",
+        "https://nile.trongrid.io"
+      );
+
+      tronWeb.setHeader({"TRON-PRO-API-KEY": '457a8d31-e0dd-4ea6-9af0-69cb00a3763e'});
+      const pk = process.env.PK;
+      const callerAddress = tronWeb.address.fromPrivateKey(pk);
+      console.log("caller", callerAddress)
+      // Workaround for https://github.com/tronprotocol/tronweb/issues/90
+      tronWeb.setAddress(callerAddress);
+    for(let batch=0; batch<5000; batch++) {
+        setTimeout(async () => {
+            let promises = [];
+            console.log("start batch ",batch, Date.now()/1000 );
+
+            for (let i = 0; i < 10; i++) {
+                promises.push(sendTx(tronWeb,pk))
+              }
+              Promise.all(promises).then(result => {
+                  writeFileSync(`${Date.now()}_result.json`, JSON.stringify(result));
+                  console.log("end batch ",batch, Date.now() );
+              }).catch(err => {
+                console.error("terminated due to error:",err.statusCode, err.statusMessage )
+              })
+              
+            }, 700*batch)
+    }
+}
+
+
 main()
-  .then((res) => console.log("success:", res))
-  .catch((err) => console.error("failed:", err));
